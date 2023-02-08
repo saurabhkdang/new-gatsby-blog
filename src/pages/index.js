8a96efc3bc5d396c3765ac3bd7261ce5f7e429f6
Saurabh Dang <saurabh.dang@technoscore.net>
2023-02-08 12:55:34 +0530

    added posts word and node version issue fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ export default ({data}) => (
   <Layout>
     <div>
       <h1>Gatsby Blog</h1>
-      <h4>{ data.allMarkdownRemark.totalCount }</h4>
+      <h4>{ data.allMarkdownRemark.totalCount } Posts</h4>
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
